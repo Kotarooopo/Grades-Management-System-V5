a347a1f94a8637d32a4b8c9c838d0b8c423adb86
Kotarooopo <134988533+Kotarooopo@users.noreply.github.com>
2024-11-18 20:06:20 +0800

    commit

--- gms/settings.py
+++ gms/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jc-4x&9xut3tnere9p3l(tup+xd0vp*_5*1#)7i$z=anndrwli'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
